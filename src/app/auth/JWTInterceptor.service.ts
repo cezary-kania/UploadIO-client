42b1d8f27467aae632a00dc0e52c0b59e3ad75b6
cezary-kania <cezary.kaniadev@gmail.com>
2021-01-08 22:48:16 +0100

    fix typos in templates

--- src/app/auth/JWTInterceptor.service.ts
+++ src/app/auth/JWTInterceptor.service.ts
@@ -5,26 +5,19 @@ import {AuthService} from './auth.service';
 
 @Injectable()
 export class JWTInterceptorService implements HttpInterceptor {
-
     constructor(private authService: AuthService) {}
-
     intercept(req: HttpRequest<any>, next: HttpHandler) {
         return this.authService.user.pipe(
             take(1),
             exhaustMap(user => {
-                if(!user) {
-                    return next.handle(req);
-                }
+                if(!user) {return next.handle(req);}
                 let token : string = null; 
                 if(req.url.endsWith('get_access_token')) token = user.refresh_token;
                 else token = user.access_token;
                 const newHeaders = req.headers.append('Authorization',`Bearer ${token}`);
-                const newRequest = req.clone({
-                    headers : newHeaders
-                });
+                const newRequest = req.clone({headers : newHeaders});
                 return next.handle(newRequest);
             })
-            
         )
     }
 }
